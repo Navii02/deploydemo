b2c1e8fb32d91d84bb93b70b94c21bc4246f8e38
Navii02 <naveenshaji02@gmail.com>
2024-05-08 23:27:23 +0530

    88/05/24

--- src/components/student/Dashboard.js
+++ src/components/student/Dashboard.js
@@ -24,7 +24,7 @@ function Dashboard() {
     const intervalId = setInterval(fetchStudentDetails, 300000); // Fetch data every 5 minutes (300000 milliseconds)
 
     return () => clearInterval(intervalId); // Clear interval on component unmount
-  }, );
+  }, []);
 
   const formatDate = (dateString) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -33,30 +33,34 @@ function Dashboard() {
 
   return (
     <>
-    <Navbar />
-    <div className="dashboard-container">
-      {student ? (
-        <div className="student-details">
-          <h4>Name: {student.name}</h4>
-          <p>Email: {student.email}</p>
-          <p>Register Number: {student.admissionNumber}</p>
-          <p>Date of Birth: {formatDate(student.dateOfBirth)}</p>
-          <p>Branch: {student.course}</p> 
-          <p>Semester: {student.semester}</p>
-          <p>Address: {student.address}</p>
-          <p>Gender: {student.gender}</p>
-          <p>Religion: {student.religion}</p>
-          <p>Caste: {student.community}</p>
-          <p>Plus Two Board: {student.plusTwo?.board}</p>
-          <p>Plus Two Mark Percentage: {student.plusTwo?.percentage}</p>
-          
-          <br/>
-          
-        </div>
-      ) : (
-        <p className="loading-text">Loading...</p>
-      )}
-    </div>
+      <Navbar />
+      <div className="dashboard-container">
+        {student ? (
+          <div className="student-details">
+            <div className="student-info">
+              <div className="student-image">
+                {student.photo && <img src={student.photo} alt="Student" className="student-photo" />}
+              </div>
+              <div className="student-text">
+                <h4>Name: {student.name}</h4>
+                <p>Email: {student.email}</p>
+                <p>Register Number: {student.admissionNumber}</p>
+                <p>Date of Birth: {formatDate(student.dateOfBirth)}</p>
+                <p>Branch: {student.course}</p>
+                <p>Semester: {student.semester}</p>
+                <p>Address: {student.address}</p>
+                <p>Gender: {student.gender}</p>
+                <p>Religion: {student.religion}</p>
+                <p>Caste: {student.community}</p>
+                <p>Plus Two Board: {student.plusTwo?.board}</p>
+                <p>Plus Two Mark Percentage: {student.plusTwo?.percentage}</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <p className="loading-text">Loading...</p>
+        )}
+      </div>
     </>
   );
 }
