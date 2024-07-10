c7f37aada94eac923094fdb5613598a21bde8b8a
Navii02 <naveenshaji02@gmail.com>
2024-07-10 11:45:21 +0530

    pro

--- front/src/ProtectedRoutes.js
+++ front/src/ProtectedRoutes.js
@@ -3,9 +3,9 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
   const email = localStorage.getItem('email');
-  const role = localStorage.getItem('role');
+ 
 
-  if (!email || !allowedRoles.includes(role)) {
+  if (!email ) {
     // If user is not logged in or doesn't have the allowed role, redirect to login
     return <Navigate to="/" />;
   }
