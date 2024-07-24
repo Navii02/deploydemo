208af188ba6f98e36c64eb4c9291e5a87e1305cd
Navii02 <naveenshaji02@gmail.com>
2024-07-24 18:59:35 +0530

    attendance1

--- back/index.js
+++ back/index.js
@@ -81,10 +81,10 @@ const Feereminder = require('./api/feereminder')
 const app = express();
 const MAX_AGE = 1000 * 60 * 60 * 3 //3hrs
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'production' ? 'https://cap-three.vercel.app' : 'http://localhost:4000',
-  optionSuccessStatus: 200,
-};
-
+  origin: 'https://cap-three.vercel.app',
+  //origin: 'http://localhost:4000',
+  optionSuccessStatus:200,
+}
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
   useNewUrlParser: true,
